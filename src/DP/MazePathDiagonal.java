594b237f1e4a6990d01b90a7db75259ad56b0470
Hanish Gupta <hghoney5@gmail.com>
2021-05-18 17:59:06 +0530

    DP.MazePathDiagonal

---
+++ src/DP/MazePathDiagonal.java
@@ -0,0 +1,109 @@
+package DP;
+
+import java.util.Arrays;
+
+public class MazePathDiagonal {
+    public static void main(String[] args) {
+        int n = 2;
+        System.out.println(MPRecursive(0,0, n, n));
+        System.out.println(MPTD(0, 0, n, n, new int[n+1][n+1]));
+        System.out.println(MPBU(n, n));
+        System.out.println(MPBUSE(n, n));
+    }
+
+    // Recursive
+    // for n = 20 it takes time. TLE
+    // TC: 2 ^ (er + ec)
+    public static int MPRecursive(int cr, int cc, int er, int ec) {
+        if(cr == er && cc == ec) {
+            return 1;
+        }
+        if(cr > er || cc > ec)
+            return 0;
+
+        int ch = MPRecursive(cr, cc + 1, er, ec);
+        int cv = MPRecursive(cr + 1, cc, er, ec);
+        int diag = MPRecursive(cr + 1, cc + 1, er, ec);
+        return ch+cv+diag;
+    }
+
+    public static int MPRecursivePrint(int cr, int cc, int er, int ec, String str) {
+        if(cr == er && cc == ec) {
+            System.out.println(str);
+            return 1;
+        }
+        if(cr > er || cc > ec)
+            return 0;
+
+        int ch = MPRecursivePrint(cr, cc + 1, er, ec, str + "H");
+        int cv = MPRecursivePrint(cr + 1, cc, er, ec, str + "V");
+        int cd = MPRecursivePrint(cr + 1, cc + 1, er, ec, str + "D");
+        return ch+cv+cd;
+    }
+
+    // TOP DOWN
+    // TC: O(er*ec)
+    // SC: O(er*ec) + Reccursion extra space
+    // fails if Stack memory fulls.
+    public static int MPTD(int cr, int cc, int er, int ec, int[][] strg) {
+        if(cr == er && cc == ec)
+            return 1;
+
+        if(cr > er || cc > ec)
+            return 0;
+
+        if(strg[cr][cc] != 0)
+            return strg[cr][cc];
+
+        int ch = MPTD(cr, cc + 1, er, ec, strg);
+        int cv = MPTD(cr + 1, cc, er, ec, strg);
+        int cd = MPTD(cr + 1, cc + 1, er, ec, strg);
+
+        strg[cr][cc] = ch + cv + cd;
+        return ch+cv+cd;
+    }
+
+    // BOTTOM UP
+    // TC: O( er * ec )
+    // SC: O( er * ec )
+    public static int MPBU(int er, int ec) {
+
+        int[][] strg = new int[er+2][ec+2];
+        for(int row = er; row >= 0; row--) {
+            for(int col = ec; col >= 0; col--) {
+
+                if(row == er && col == ec) {
+                    strg[row][col] = 1;
+                }
+                else {
+                    strg[row][col] = strg[row][col + 1] + strg[row + 1][col] + strg[row + 1][col + 1];
+                }
+            }
+        }
+
+        return strg[0][0];
+    }
+
+    // BOTTOM UP SPACE EFFICIENT
+    // we use only a row and update its contents like sliding bottom to up from the previous MPBU approach strg.
+    // TC: O( er * ec )
+    // SC: O( ec )
+    public static int MPBUSE(int er, int ec) {
+        int[] strg = new int[ec + 1];
+        int diag = 0;
+        Arrays.fill(strg, 1);
+
+        for(int i=er-1; i>=0; i--) {
+            for(int col = ec-1; col>=0; col--) {
+                if(col == ec-1)
+                    diag = 1;
+                int val = strg[col] + strg[col + 1] + diag;
+                diag = strg[col];
+                strg[col] = val;
+            }
+        }
+
+
+        return strg[0];
+    }
+}
